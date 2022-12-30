98a1bef2d82a8c1d3ae377a412c589557a3cdb71
“guchondayo” <uto.to.be.engineer@gmail.com>
2022-12-30 12:25:53 +0900

    とりま根本的な解決にはなっていないけど

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
-<script>
-import InsertTest from './components/InsertTest.vue'
-
-export default {
-  name: 'App',
-  components: {
-    InsertTest
+<template>
+  <div id="app">
+      <p>このコンポーネントを継承することができました！</p>
+      <p>つまり、export-defaultが必要なときは、dataをつかったり、他のコンポーネントを引っ張ってきたり。そういうことじゃないかな</p>
+  </div>
+</template>
+<style>
+  p{
+      color: brown;
   }
-}
-</script>
+</style>
 
