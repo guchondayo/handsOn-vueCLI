10579cc0efed575110871ed51e6dded74e1c3144
“guchondayo” <uto.to.be.engineer@gmail.com>
2023-01-01 16:22:13 +0900

    最低限必要な実装

--- src/App.vue
+++ src/App.vue
@@ -3,25 +3,6 @@
 <!-- タグ忘れがち -->
 <template>
   <div id="app">
-    <p>{{ titleCom }}</p>
-    <p>{{ checkPro }}</p>
-    <HeaderComponents name="谷口"></HeaderComponents>
+    <p>成功</p>
   </div>
 </template>
-
-<script>
-// 継承したい時は、前回と同じように、テンプレートを継承しよう
-import HeaderComponents from "./components/HeaderComponents.vue"
-// 継承したい時は、インポートしたcomponentを使って、テンプレートを継承しよう
-export default{
-  data:function(){
-    return {
-      titleCom:"data成功",
-      checkPro:"prop始めました",
-    }
-  },
-  components: {
-    HeaderComponents
-  }
-}
-</script>
