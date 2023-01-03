8334b3088819a82c060b6e245ad81c8e943817c0
“guchondayo” <uto.to.be.engineer@gmail.com>
2023-01-03 10:33:45 +0900

    ライフサイクルフックが動くことを確認

--- src/components/HeaderComponents.vue
+++ src/components/HeaderComponents.vue
@@ -8,13 +8,19 @@ export default{
     data:function(){
         return {
             checkData:"成功",
-            // postData:"",
-            // postURL:"https://zipcloud.ibsnet.co.jp/api/search"
+            postData:"",
+            postURL:"https://zipcloud.ibsnet.co.jp/api/search",
+            result:""
         }
     },
     created() {
         console.log("created",this.checkData,"DOMを読み取り前",this.$el,"要素が取れない")
-
+        fetch(this.postURL + "?zipcode=2520318").then(function(res){
+            return res.json()    
+        }).then(function(data){
+            console.log(data.results[0])
+        })
+        console.log("mounted",this.checkData,"DOMを読み取り後",this.$el,"要素が取れる")
     },
     mounted(){
         console.log("mounted",this.checkData,"DOMを読み取り後",this.$el,"要素が取れる")
